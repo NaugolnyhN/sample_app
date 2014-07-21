b751c529f2437527f0899f15216a179d8b71a3c8
NaugolnyhN <naugolnyh.n@gmail.com>
2014-07-21 19:04:41 +0400

    Finish user edit, update, index, and destroy actions

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,65 +1,56 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  subject { page }
 
-  describe "Home page" do
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
 
-    it "should have the h1 'Sample App'" do
-      visit root_path
-      expect(page).to have_content('Sample App')
-    end
+   describe "Home page" do
+    before { visit root_path }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
 
-    it "should have the base title" do
-      visit root_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
-    end
-
-    it "should not have a custom page title" do
-      visit root_path
-      expect(page).not_to have_title('| Home')
-    end
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the h1 'Help'" do
-      #visit help_path
-      visit root_path
-      expect(page).to have_content('Help')
-    end
-
-    it "should have the title 'Help'" do
-      #visit help_path
-      visit help_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
-    end
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
+    before { visit about_path }
 
-    it "should have the h1 'About Us'" do
-      visit about_path
-      #visit root_path
-      expect(page).to have_content('About Us')
-    end
-
-    it "should have the title 'About Us'" do
-      visit about_path
-      #visit root_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
-    end
+    
+    it { should have_selector('h1', text: 'About') }
+    it { should have_title(full_title('About Us')) } 
   end
 
-  describe "Contact page" do
+describe "Contact page" do
+    before { visit contact_path }
 
-    it "should have the content 'Contact'" do
-      visit root_path
-      expect(page).to have_content('Contact')
-    end
+    it { should have_selector('h1', text: 'Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
 
-    it "should have the title 'Contact'" do
-      visit root_path
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
-    end
+it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample app"
+    expect(page).to have_selector('h1', text: 'Sample App')
   end
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,12 +1,12 @@
 include ApplicationHelper
 
-def full_title(page_title)
-  base_title = "Ruby on Rails Tutorial Sample App"
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
+#def full_title(page_title)
+ # base_title = "Ruby on Rails Tutorial Sample App"
+ # if page_title.empty?
+  #  base_title
+  #else
+  #  "#{base_title} | #{page_title}"
+  #end
 def sign_in(user, options={})
   if options[:no_capybara]
     # Sign in when not using Capybara.
@@ -20,4 +20,4 @@ def sign_in(user, options={})
     click_button "Sign in"
   end
 end
-end
+#end
